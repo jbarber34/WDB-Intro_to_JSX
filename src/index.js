a9691778a7374e19a471a9b4a3dd20d41af353bb
James Barber <38710518+jbarber34@users.noreply.github.com>
2022-05-22 15:24:51 -0400

    Update to intro to JSX How to use HTML inside JavaScript

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const React = require("react");
+const ReactDOM = require("react-dom");
+
+ReactDOM.render(
+  <div>
+    <h1>Hello World!</h1>
+    <p> This is a paragraph </p>
+  </div>,
+  document.getElementById("root")
+);
